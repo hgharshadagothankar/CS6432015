e5c5877cc77412c7add09a19979caa75f05d1ae9
hgharshadagothankar@gmail.com <hgharshadagothankar@gmail.com>
2015-09-30 15:58:22 -0400

    iojoii

---
+++ TwilioPro_1/src/twilio/TwilioServlet.java
@@ -0,0 +1,77 @@
+package twilio;
+
+//Replying code
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+ 
+import com.twilio.sdk.verbs.TwiMLResponse;
+import com.twilio.sdk.verbs.TwiMLException;
+import com.twilio.sdk.verbs.Message;
+ 
+public class TwilioServlet extends HttpServlet {
+ 
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Create a dict of people we know.
+        HashMap<String, String> callers = new HashMap<String, String>();
+        //917 216 4313
+        callers.put("+15515879364", "Harshada");
+        /*callers.put("+14158675310", "Boots");
+        callers.put("+14158675311", "Virgil");*/
+ 
+        String fromNumber = request.getParameter("From");
+        String knownCaller = callers.get(fromNumber);
+        String message;
+        if (knownCaller == null) {
+            // Use a generic message
+            message = "Please register your number";
+        } else {
+            // Use the caller's name
+            message = knownCaller + "hello from cs6432015 from Harshada Gothankar";
+        }
+ 
+        // Create a TwiML response and add our friendly message.
+        TwiMLResponse twiml = new TwiMLResponse();
+        Message sms = new Message(message);
+        try {
+            twiml.append(sms);
+        } catch (TwiMLException e) {
+            e.printStackTrace();
+        }
+ 
+        response.setContentType("application/xml");
+        response.getWriter().print(twiml.toXML());
+    }
+}
+/*import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+ 
+import com.twilio.sdk.verbs.TwiMLResponse;
+import com.twilio.sdk.verbs.TwiMLException;
+import com.twilio.sdk.verbs.Message;
+ 
+public class TwilioServlet extends HttpServlet {
+ 
+    // service() responds to both GET and POST requests.
+    // You can also use doGet() or doPost()
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        TwiMLResponse twiml = new TwiMLResponse();
+        Message message = new Message("Hello, Mobile Monkey");
+        PrintWriter out=response.getWriter();
+      //  out.println("HIIIIIIII");
+        try {
+            twiml.append(message);
+        } catch (TwiMLException e) {
+            e.printStackTrace();
+        }
+ 
+        response.setContentType("application/xml");
+        response.getWriter().print(twiml.toXML());
+    }
+}
+*/
